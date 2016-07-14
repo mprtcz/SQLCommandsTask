96ccad8a3d24e4f205d0263002ca1e5bd74a7ba0
Mordin <azet89@gmail.com>
2016-07-14 16:45:45 +0200

    deleed files

--- select2.sql
+++
@@ -1,8 +0,0 @@
-.open "database.db"
-
-.print "Selecting names of the countries that have no buildings at all"
-
-SELECT Name FROM Country WHERE CountryID NOT IN (
-	SELECT CountryID FROM City WHERE CityID IN(
-	SELECT CityID FROM Building GROUP BY CityID)
-		GROUP BY CountryID) GROUP BY Name;
